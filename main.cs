4072a6607c754db1dcd5861a3c77bfa349a69553
16macudaS <102147501+SzymonComp@users.noreply.github.com>
2022-03-22 09:37:18 +0000

    Added Titanic (This is Africa)

--- main.cs
+++ main.cs
@@ -1,9 +1,15 @@
 using System;
 
-
-
 class Program {
   public static void Main (string[] args) {
+    static void Titanic()
+    {
+      Console.WriteLine("Tsamina mina, eh eh");
+      Console.WriteLine("Waka waka, eh eh");
+      Console.WriteLine("Tsamina mina zangalewa");
+      Console.WriteLine("This time for Africa");
+    }
+    Titanic();
     Console.WriteLine ("Hello World");
   }
 }
